fc1837b6298fe126a9fd5e20f5e3d4fbb5c0ab28
LakshyaChaudhry <lakshchaudhry@gmail.com>
2026-03-01 18:16:16 -0500

    makemore.py added

---
+++ makemore.py
@@ -0,0 +1,3 @@
+#This is my replication of Andrej Karpathy's makemore project to understand how language models work, and are built from scratch.
+#There is no AI or auto complete used in this project.
+
